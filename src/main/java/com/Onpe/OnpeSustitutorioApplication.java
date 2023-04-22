3d67a515812225473e662551f30256c64027a573
PC <PC@SCCTTO71361935>
2023-04-21 19:50:39 -0500

    sustitutorio

---
+++ src/main/java/com/Onpe/OnpeSustitutorioApplication.java
@@ -0,0 +1,13 @@
+package com.Onpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnpeSustitutorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnpeSustitutorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Onpe/OnpeSustitutorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Onpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnpeSustitutorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
