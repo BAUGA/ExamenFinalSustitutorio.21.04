e9679f8e48f3c495fe94d3f691112d0a9e28fcdc
PC <PC@SCCTTO71361935>
2023-04-21 20:16:12 -0500

    agregando

--- src/main/java/com/Onpe/model/Votacion.java
+++ src/main/java/com/Onpe/model/Votacion.java
@@ -1,5 +1,54 @@
 package com.Onpe.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Votacion {
+	@Id
+	private int idLocalVotacion;
+	private String Departamento;
+	private String Provincia;
+	private String Distrito;
+	private String RazonSocial;
+	private String Direccion;
+	public int getIdLocalVotacion() {
+		return idLocalVotacion;
+	}
+	public void setIdLocalVotacion(int idLocalVotacion) {
+		this.idLocalVotacion = idLocalVotacion;
+	}
+	public String getDepartamento() {
+		return Departamento;
+	}
+	public void setDepartamento(String departamento) {
+		Departamento = departamento;
+	}
+	public String getProvincia() {
+		return Provincia;
+	}
+	public void setProvincia(String provincia) {
+		Provincia = provincia;
+	}
+	public String getDistrito() {
+		return Distrito;
+	}
+	public void setDistrito(String distrito) {
+		Distrito = distrito;
+	}
+	public String getRazonSocial() {
+		return RazonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		RazonSocial = razonSocial;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	
 
 }
--- src/main/java/com/Onpe/repository/IVotacion.java
+++ src/main/java/com/Onpe/repository/IVotacion.java
@@ -1,5 +1,16 @@
 package com.Onpe.repository;
 
-public interface IVotacion {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.Onpe.model.Votacion;
+
+
+@Repository
+
+public interface IVotacion  extends JpaRepository<Votacion, String> {
+	@Query(value= "{call usp_getGrupoVotacion(:id)}",nativeQuery=true)
+Votacion getGrupoVotacion(String id);
+	
 }
